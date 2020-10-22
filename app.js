29e3c8bc48c46249ad8d446e6346edc32d18ea94
Lucia <lubersol@gmail.com>
2020-10-22 16:44:22 +0200

    eliminacion pagina html, añadido package.json

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 //Aplicación con funciones para conectar con el backend
+// let upperCase = require('upper-case');
+// console.log(upperCase.upperCase('hello world!'));
+//EJERCICIO FACTORIAL 
+// let res = 1;
+// for(let i=1 ;i<=n; i++) {
+//     res= res*i;
+// }
+// return resultado;
+//SOLUCION RECURSIVA DE LA MISMA FUNCION DE FACTORIAL
+// (n)=> {
+//     if (n === 1) return 1;
+//     else return f(n - 1) * n;
+// };
+// n(3);
+const express = require('express');
+const app = express();
+//Instanciamos el servidor
+app.listen(3000,()=>console.log('Servidor levantado en 3000')); 
+//Routing con express
+//const Coches = {Volvo: 'txt'};
+app.get('/request/coches', (req, res)=> {
+   //return res.send(arrayCoches);
+   //return res.send('Hello World');
+   res.send('The car is: '+ req.query.coches);
+});
