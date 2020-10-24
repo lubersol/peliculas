d5b2ae10e6985621f404078f1d00269fa7f6aac0
Lucia <lubersol@gmail.com>
2020-10-24 13:06:40 +0200

    creacion array peliculas

--- app.js
+++ app.js
@@ -1,26 +1,54 @@
-//Aplicación con funciones para conectar con el backend
-// let upperCase = require('upper-case');
-// console.log(upperCase.upperCase('hello world!'));
-//EJERCICIO FACTORIAL 
-// let res = 1;
-// for(let i=1 ;i<=n; i++) {
-//     res= res*i;
-// }
-// return resultado;
-//SOLUCION RECURSIVA DE LA MISMA FUNCION DE FACTORIAL
-// (n)=> {
-//     if (n === 1) return 1;
-//     else return f(n - 1) * n;
-// };
-// n(3);
+//PARA IMPORTAR E INVOCAR EL MODULO DE EXPRESS
 const express = require('express');
 const app = express();
-//Instanciamos el servidor
-app.listen(3000,()=>console.log('Servidor levantado en 3000')); 
-//Routing con express
-//const Coches = {Volvo: 'txt'};
-app.get('/request/coches', (req, res)=> {
-   //return res.send(arrayCoches);
-   //return res.send('Hello World');
-   res.send('The car is: '+ req.query.coches);
+
+const PORT = 3000;
+//MIDDLEWARE
+app.use(express.json());
+
+//INSTANCIAMOS EL SERVIDOR
+app.listen(3000, () => console.log('Servidor levantado en 3000'));
+
+//Creamos array con objetos de peliculas
+
+const peliculas = [
+    {id:'1', titulo:'Match point', director:'Woody Allen', interprete:'Jonathan Rhys Meyers'},
+    {id:'2', titulo:'Orgullo y prejuicio', director:'Joe Wright', interprete:'Keira Knightley'},
+    {id:'3', titulo:'Blade runner', director:'Ridley Scott', interprete:'Harrison Ford'},
+    {id:'4', titulo:'Infiltrados', director:'Martin Scorsese', interprete:'Leonardo Di Caprio'},
+    {id:'5', titulo:'Shame', director:'Steve McQueen', interprete:'Michael Fassbender'},
+    {id:'6', titulo:'Muerte entre las flores', director:'Ethan Coen', interprete:'Gabriel Byrne'},
+    {id:'7', titulo:'Pulp fiction', director:'Quentin Tarantino', interprete:'John Travolta'},
+    {id:'8', titulo:'Malditos bastardos', director:'Quentin Tarantino', interprete:'Christoph Waltz'},
+    {id:'9', titulo:'Tal como eramos', director:'Sydney Pollack', interprete:'Robert Redford'},
+    {id:'10', titulo:'Melancolia', director:'Lars Von Triers', interprete:'Kirsten Dunst'},
+    {id:'11', titulo:'2046', director:'Won Kar-Wai', interprete:'Zhang Ziyi'},
+    {id:'12', titulo:'Canino', director:'Yorgos Lanthimos', interprete:'Mary Tsoni'}
+]
+
+//HTTP://LOCALHOST:3000/ (escribimos eso en el la barra del navegador)
+app.get('/', (req, res) => {
+    res.send('Hello world');
 });
+
+//Endpoint para la ruta peliculas (todas)
+app.get('/peliculas', (req, res) => {
+    res.json('ALL');
+});
+
+//Endpoint para la ruta de pelicula (id)
+app.get('/pelicula/:id', (req, res) => {
+let { id } = req.params;
+    res.json(`${id}`);
+});
+
+//Endpoint para la ruta de queries
+app.get('/pelicula', (req, res) => {
+let { q } = req.query;
+res.json(`${q}`);
+});
+
+
+
+
+
