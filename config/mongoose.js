89cbb2ef32c2e75413f4af44f7bb0eba0feb320d
Lucia <lubersol@gmail.com>
2020-11-12 19:45:16 +0100

    heroku

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const MongoURI = process.env.MongoURI || 'mongodb://localhost:27017/heroku-mongo'
 mongoose.connect(MongoURI, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('connected to Mongodb: ' + MongoURI));
+    .then(() => console.log('connected to Mongodb: ' + MongoURI))
+    .catch(e=>console.error('mongoose erroneo', e))
 
-module.exports = mongoose('mongoose');
+
