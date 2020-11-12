3f11583b98be6ec3fa9de8aa6eabe1685a2a9632
Lucia <lubersol@gmail.com>
2020-11-12 17:10:52 +0100

    heroku

--- app.js
+++ app.js
@@ -12,14 +12,6 @@ const routesOrder = require('./components/order/router.js');
 //PUERTO
 const PORT = process.env.PORT || 3000;
 
-//CONEXION A LA BASE DE DATOS
-// const mongoose = require('mongoose');
-// const MongoURI = process.env.MongoURI || 'mongodb://localhost:27017/heroku-mongo'
-// mongoose.connect(MongoURI, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log('connected to Mongodb: ' + MongoURI));
-    //PARA VER SI ESTAMOS CONECTADOS A LA BD
-
-
 //MIDDLEWARE PARA RUTAS
 app.use(express.json());
 app.use('/api/user', routesUser);
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,6 @@
-const mongoose= require ('mongoose');
-const MongoURI = process.env.MongoURI ||'mongodb://localhost:27017/heroku-mongo'
+const mongoose = require('mongoose');
+const MongoURI = process.env.MongoURI || 'mongodb://localhost:27017/heroku-mongo'
 mongoose.connect(MongoURI, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('connected to Mongodb: ' + MongoURI));
+
+module.exports = mongoose('mongoose');
